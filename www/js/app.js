fc98845cba6c980c2d7a08c04586c3d25f20c1aa
hounenhounen <hounen.hounen@gmail.com>
2015-12-29 23:46:49 +0900

    Commit5

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,22 @@ function CurrentPoint(){
     geopoint=null;  //端末の位置情報を保持する
 }
 
+//登録されたポイントを引き出し地図上に表示する
+function find_geopoint(){
+    navigator.geolocation.getCurrentPosition(onFindSuccess, onGeoError, geoOption);
+    console.log("find_geopoint");
+}
+    
+//登録ポイントの表示時に位置情報取得に成功した場合のコールバック
+var onFindSuccess = function(location){
+        current.geopoint = location.coords; 
+        var geoPoint = new ncmb.GeoPoint(location.coords.latitude, location.coords.longitude);
+        console.log("findpoints:"+location.coords.latitude + ":" + location.coords.longitude);
+        
+        
+        
+    };
+
 //現在地をポイントとして登録する
  function save_geopoint(){
      navigator.geolocation.getCurrentPosition(onSaveSuccess, onGeoError, geoOption);
@@ -78,51 +94,11 @@ var onSaveSuccess = function(location){
             current.geopoint = location.coords; 
             var geoPoint = new ncmb.GeoPoint(location.coords.latitude, location.coords.longitude);
             console.log(location.coords.latitude + ":" + location.coords.longitude);
-            var Places = ncmb.DataStore("PlacePoints");
-            var point = new Places();
-            point.set("name",results.input1);
-            point.set("geo", geoPoint);
-        
-            point.save()
-            .then(function(){})
-            .catch(function(err){// エラー処理
-            });
+            
+            
+            
         }
         
     };
 
-//登録されたポイントを引き出し地図上に表示する
-function find_geopoint(){
-    navigator.geolocation.getCurrentPosition(onFindSuccess, onGeoError, geoOption);
-    console.log("find_geopoint");
-}
-    
-//登録ポイントの表示時に位置情報取得に成功した場合のコールバック
-var onFindSuccess = function(location){
-        current.geopoint = location.coords; 
-        var geoPoint = new ncmb.GeoPoint(location.coords.latitude, location.coords.longitude);
-        console.log("findpoints:"+location.coords.latitude + ":" + location.coords.longitude);
-        var PlacePointsClass = ncmb.DataStore("PlacePoints");
-        //ニフティクラウド mobile backendにアクセスして検索開始位置を指定
-        PlacePointsClass.withinKilometers("geo", geoPoint, 5)
-                        .fetchAll()
-                        .then(function(results){
-                            var data = [];
-                            for (var i = 0; i < results.length; i++) {
-                                var result = results[i];
-                                var markers = new OpenLayers.Layer.Markers("Markers");
-                                map.addLayer(markers);
-                                var regist_location = result.get("geo");
-                                var marker = new OpenLayers.Marker(
-                                    new OpenLayers.LonLat(regist_location.longitude,regist_location.latitude)
-                                                .transform(
-                                                new OpenLayers.Projection("EPSG:4326"), 
-                                                new OpenLayers.Projection("EPSG:900913")
-                                            )
-                                );
-                                markers.addMarker(marker);
-                            }
-                        });
-        
-    };
     
