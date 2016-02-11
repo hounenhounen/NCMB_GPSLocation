c0d0de7243d0f9562e39df6b98fb98b015bde356
hounenhounen <kawahara@kawahara-no-MacBook-Air.local>
2016-02-11 22:24:57 +0900

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -70,7 +70,27 @@ var onFindSuccess = function(location){
         var geoPoint = new ncmb.GeoPoint(location.coords.latitude, location.coords.longitude);
         console.log("findpoints:"+location.coords.latitude + ":" + location.coords.longitude);
         
-        
+        var PlacePointsClass = ncmb.DataStore("PlacePoints");
+        //ニフティクラウド mobile backendにアクセスして検索開始位置を指定
+        PlacePointsClass.withinKilometers("geo", geoPoint, 5)
+                        .fetchAll()
+                        .then(function(results){
+                            var data = [];
+                            for (var i = 0; i < results.length; i++) {
+                                  var result = results[i];
+                                  var markers = new OpenLayers.Layer.Markers("Markers");
+                                  map.addLayer(markers);
+                                  var regist_location = result.get("geo");
+                                  var marker = new OpenLayers.Marker(
+                                      new OpenLayers.LonLat(regist_location.longitude,regist_location.latitude)
+                                                  .transform(
+                                                  new OpenLayers.Projection("EPSG:4326"), 
+                                                  new OpenLayers.Projection("EPSG:900913")
+                                              )
+                                  );
+                                  markers.addMarker(marker);
+                              }
+                          });
         
     };
 
@@ -95,7 +115,15 @@ var onSaveSuccess = function(location){
             var geoPoint = new ncmb.GeoPoint(location.coords.latitude, location.coords.longitude);
             console.log(location.coords.latitude + ":" + location.coords.longitude);
             
-            
+            var Places = ncmb.DataStore("PlacePoints");
+            var point = new Places();
+            point.set("name",results.input1);
+            point.set("geo", geoPoint);
+
+            point.save()
+            .then(function(){})
+            .catch(function(err){// エラー処理
+            });
             
         }
         
